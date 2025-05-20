c71a5f3d724e66f76df3339c4fe9519c9199a6ec
sergpolozun <sergpolozuns@gmail.com.>
2025-05-20 20:49:05 +0300

    добавление shopa, переделал архитектуру

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
 
-
-app_name = 'main'
-
 urlpatterns = [
-    path('', views.popular_list, name='popular_list'),
-    path('<slug:slug>/', views.product_detail, name='popular_detail')
+    path('', views.desktop_view, name='desktop'),  # или другое название твоей вьюшки
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from django.shortcuts import render
 
-def popular_list(request):
-    products = Product.objects.filter(available=True)[:3]
-    return render(request, 
-                  'main/index/index.html', 
-                  {'products': products})
-
-def product_detail(request, slug):
-    product = get_object_or_404(Product, 
-                                slug=slug,
-                                available=True)
-    return render(request,
-                  'main/index/detail.html',
-                  {'product': product})
-
+def desktop_view(request):
+    return render(request, 'main/index/index.html')
